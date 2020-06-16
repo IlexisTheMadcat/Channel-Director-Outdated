72e6482a1ed7f9cccf6fc53dce6e1dd91111b50f
thede <thedestoryer11@gmail.com>
2020-06-16 12:15:28 -0500

    Context-managerized LoadingUpdate so Rem won't be such a b**** for no reason

--- utils/directory_mgmt.py
+++ utils/directory_mgmt.py
@@ -1,7 +1,7 @@
 
 # Lib
 from typing import List
-
+from contextlib import contextmanager
 # Site
 
 # Local
@@ -12,3 +12,12 @@ def recurse_index(d: dict, indices: List[str]):
     for index in indices:
         ret = ret[index]
     return ret
+
+
+@contextmanager
+def LoadingUpdate_contextmanager(bot, ID: int):
+    bot.univ.LoadingUpdate.append(ID)
+    try:
+        yield None
+    finally:
+        bot.univ.LoadingUpdate.remove(ID)
