970941f50149a2187f8404fe09041e279bfe4db1
SUPER MECH M500 <thedestoryer11@gmail.com>
2020-06-30 01:27:40 -0400

    Add docstrings and new context manager

--- utils/directory_mgmt.py
+++ utils/directory_mgmt.py
@@ -15,9 +15,36 @@ def recurse_index(d: dict, indices: List[str]):
 
 
 @contextmanager
-def LoadingUpdate_contextmanager(bot, ID: int):
-    bot.univ.LoadingUpdate.append(ID)
+def loadingupdate(bot, g_id: int):
+    """
+    Prevents Rem from having a mental breakdown when commands are spammed.
+    Removes the guild from the list on completion
+
+    :param bot: The Bot class
+    :param g_id: The Guild.id to append to the list
+    :return: None
+    """
+    bot.univ.LoadingUpdate.append(g_id)
     try:
         yield None
     finally:
-        bot.univ.LoadingUpdate.remove(ID)
+        bot.univ.LoadingUpdate.remove(g_id)
+
+
+@contextmanager
+def usinggui(bot, g_id: int, u_id: int):
+    """
+    Users using the GUI; Restricts to one GUI action/user at a time
+    Removes the user from the list on completion
+
+    :param bot: The Bot class
+    :param g_id: The Guild.id that the GUI belongs to.
+    :param u_id: The User.id who is using the GUI.
+    :return: None
+    """
+
+    bot.univ.using_gui.update({g_id: u_id})
+    try:
+        yield None
+    finally:
+        bot.univ.using_gui.pop(g_id)
